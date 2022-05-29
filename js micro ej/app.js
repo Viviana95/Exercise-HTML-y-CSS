437dcc249fa94e42ff11242ffb90b5f89a529070
Viviana <vivitr5195@gmail.com>
2022-05-29 16:58:33 +0200

    ej js

--- js micro ej/app.js
+++ js micro ej/app.js
@@ -1,16 +1,16 @@
 //Mostrar en pantalla Hello World
+let message="Hello World";
+document.write(message);
 
-document.write("Hello World");
 
+document.write("<br>");
 //mostrar resultado de sumar 3+5
 let num1= 3;
 let num2= 5;
+let sumar=num1+num2;
 
+document.write("La suma de 3+5 es:"+sumar);
 
-function sumar()
-{
-    console.log('num1 + num2 "=8"');
-}
 
 
 
---
+++ js micro ej/app1.js
@@ -0,0 +1,14 @@
+
+//ej 3
+let user=prompt('INTRODUZCA SU NOMBRE');
+alert ('Hola '+user);
+
+
+ //ej 4  
+ function Sumar() {
+let n1 = document.getElementById('number1').value;
+let n2 = document.getElementById('number2').value;
+let sum = parseInt(n1) + parseInt(n2);
+
+   alert("El resultado es: "+sum)
+}
